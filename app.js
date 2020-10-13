3c7bf30bb1281b6985877d287a60d37df1bacb99
krishnadavuluri <60300403+krishnadavuluri@users.noreply.github.com>
2020-10-13 09:26:10 +0530

    Changes done

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ function GetReport()
         PlaceName.style.visibility="visible";
         Temperature.innerHTML=Math.floor(temp-273);
         //second change.
+        
         unit.style.visibility="visible";
         Temperature.style.visibility="visible";
         //this is new commit.
